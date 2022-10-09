67f11402ab5834fa213ba32784c99b5a87f32450
Anastasiya Gaizhevska <vindy.my@gmail.com>
2022-10-09 20:32:54 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,316 @@
+//    Я знаю, що це все мало бути загорнуто у функції і скорочено до мінімального мінімуму,
+// але не вистачило знань, та ще й моя голова зовсім відмовилась зі мною співпрацювати пізно ввечері :'(
+   
+   let falseLink1 = document.querySelector(".false-link1");
+    falseLink1.addEventListener("click", changeInner1);
+
+     function changeInner1(event) {
+        event.preventDefault();
+        let falseLink = document.querySelector(".false-link1");
+        falseLink.outerHTML = `Running predict()...<br>
+Using seed: 7825 <br>
+Generating initial latents for 15 steps <br>
+Generating frame 0 <br>
+Generating frame 1 <br>
+Generating frame 2 <br>
+Generating frame 3 <br>
+Generating frame 4 <br>
+Generating frame 5 <br>
+Generating frame 6 <br>
+Generating frame 7 <br>
+Generating frame 8 <br>
+Generating frame 9 <br>
+Generating frame 10 <br>
+Generating frame 11 <br>
+Generating frame 12 <br>
+Generating frame 13 <br>
+Generating frame 14 <br>
+Generating frame 15 <br>
+Generating frame 16 <br>
+Generating frame 17 <br>
+Generating frame 18 <br>
+Generating frame 19 <br>
+Interpolating images with FILM <br>
+2022-08-28 20:59:12.761120: Itensorflow/stream_executor/cuda/cuda_dnn.cc:368] Loaded cuDNN version 8400 <br>
+Saving MP4`;
+      }
+
+      let falseLink2 = document.querySelector(".false-link2");
+    falseLink2.addEventListener("click", changeInner2);
+
+     function changeInner2(event) {
+        event.preventDefault();
+        let falseLink = document.querySelector(".false-link2");
+        falseLink.outerHTML = `Running predict()...<br>
+Using seed: 41784 <br>
+Generating initial latents for 15 steps<br>
+Generating frame 0<br>
+Generating frame 1<br>
+Generating frame 2<br>
+Generating frame 3<br>
+Generating frame 4<br>
+Generating frame 5<br>
+Generating frame 6<br>
+Generating frame 7<br>
+Generating frame 8<br>
+Generating frame 9<br>
+Generating frame 10<br>
+Generating frame 11<br>
+Generating frame 12<br>
+Generating frame 13<br>
+Generating frame 14<br>
+Generating frame 15<br>
+Generating frame 16<br>
+Generating frame 17<br>
+Generating frame 18<br>
+Generating frame 19<br>
+Interpolating images with FILM<br>
+Saving MP4`;
+      }
+
+      let falseLink3 = document.querySelector(".false-link3");
+    falseLink3.addEventListener("click", changeInner3);
+
+     function changeInner3(event) {
+        event.preventDefault();
+        let falseLink = document.querySelector(".false-link3");
+        falseLink.outerHTML = `Running predict()...<br>
+Using seed: 58697<br>
+Generating initial latents for 4 steps<br>
+Generating frame 0<br>
+Generating frame 1<br>
+Generating frame 2<br>
+Generating frame 3<br>
+Generating frame 4<br>
+Generating frame 5<br>
+Generating frame 6<br>
+Generating frame 7<br>
+Generating frame 8<br>
+Generating frame 9<br>
+Generating frame 10<br>
+Generating frame 11<br>
+Generating frame 12<br>
+Generating frame 13<br>
+Generating frame 14<br>
+Generating frame 15<br>
+Generating frame 16<br>
+Generating frame 17<br>
+Generating frame 18<br>
+Generating frame 19<br>
+Interpolating images with FILM<br>
+2022-08-28 21:00:36.361342: I tensorflow/stream_executor/cuda/cuda_dnn.cc:368] Loaded cuDNN version 8400<br>
+Saving MP4`;
+      }
+    
+       let falseLink4 = document.querySelector(".false-link4");
+    falseLink4.addEventListener("click", changeInner4);
+
+     function changeInner4(event) {
+        event.preventDefault();
+        let falseLink = document.querySelector(".false-link4");
+        falseLink.outerHTML = `Running predict()...<br>
+Using seed: 52273<br>
+Generating initial latents for 4 steps<br>
+Generating frame 0<br>
+Generating frame 1<br>
+Generating frame 2<br>
+Generating frame 3<br>
+Generating frame 4<br>
+Generating frame 5<br>
+Generating frame 6<br>
+Generating frame 7<br>
+Generating frame 8<br>
+Generating frame 9<br>
+Generating frame 10<br>
+Generating frame 11<br>
+Generating frame 12<br>
+Generating frame 13<br>
+Generating frame 14<br>
+Generating frame 15<br>
+Generating frame 16<br>
+Generating frame 17<br>
+Generating frame 18<br>
+Generating frame 19<br>
+Generating frame 20<br>
+Generating frame 21<br>
+Generating frame 22<br>
+Generating frame 23<br>
+Generating frame 24<br>
+Interpolating images with FILM<br>
+Saving MP4`;
+      }
+
+      let falseLink5 = document.querySelector(".false-link5");
+    falseLink5.addEventListener("click", changeInner5);
+
+     function changeInner5(event) {
+        event.preventDefault();
+        let falseLink = document.querySelector(".false-link5");
+        falseLink.outerHTML = `Running predict()...<br>
+Using seed: 42681<br>
+Generating initial latents for 15 steps<br>
+Generating frame 0<br>
+Generating frame 1<br>
+Generating frame 2<br>
+Generating frame 3<br>
+Generating frame 4<br>
+Generating frame 5<br>
+Generating frame 6<br>
+Generating frame 7<br>
+Generating frame 8<br>
+Generating frame 9<br>
+Generating frame 10<br>
+Generating frame 11<br>
+Generating frame 12<br>
+Generating frame 13<br>
+Generating frame 14<br>
+Generating frame 15<br>
+Generating frame 16<br>
+Generating frame 17<br>
+Generating frame 18<br>
+Generating frame 19<br>
+Generating frame 20<br>
+Generating frame 21<br>
+Generating frame 22<br>
+Generating frame 23<br>
+Generating frame 24<br>
+Generating frame 25<br>
+Generating frame 26<br>
+Generating frame 27<br>
+Generating frame 28<br>
+Generating frame 29<br>
+Generating frame 30<br>
+Generating frame 31<br>
+Generating frame 32<br>
+Generating frame 33<br>
+Generating frame 34<br>
+Generating frame 35<br>
+Generating frame 36<br>
+Generating frame 37<br>
+Generating frame 38<br>
+Generating frame 39<br>
+Interpolating images with FILM<br>
+Saving MP4`;
+      }
+
+       let falseLink6 = document.querySelector(".false-link6");
+    falseLink6.addEventListener("click", changeInner6);
+
+     function changeInner6(event) {
+        event.preventDefault();
+        let falseLink = document.querySelector(".false-link6");
+        falseLink.outerHTML = `Running predict()...<br>
+Using seed: 21704<br>
+Generating initial latents for 12 steps<br>
+Generating first frame<br>
+Generating last frame<br>
+Generating frame 1<br>
+Generating frame 2<br>
+Generating frame 3<br>
+Generating frame 4<br>
+Generating frame 5<br>
+Generating frame 6<br>
+Generating frame 7<br>
+Generating frame 8<br>
+Interpolating images with FILM<br>
+2022-08-29 17:04:52.767686: I tensorflow/stream_executor/cuda/cuda_dnn.cc:368] Loaded cuDNN version 8400<br>
+Saving MP4`;
+      }
+
+
+        let falseLink7 = document.querySelector(".false-link7");
+    falseLink7.addEventListener("click", changeInner7);
+
+     function changeInner7(event) {
+        event.preventDefault();
+        let falseLink = document.querySelector(".false-link7");
+        falseLink.outerHTML = `Running predict()...<br>
+Using seed: 25381<br>
+Generating initial latents for 4 steps<br>
+Generating first frame<br>
+Generating last frame<br>
+Generating frame 1<br>
+Generating frame 2<br>
+Generating frame 3<br>
+Generating frame 4<br>
+Generating frame 5<br>
+Generating frame 6<br>
+Generating frame 7<br>
+Generating frame 8<br>
+Interpolating images with FILM<br>
+2022-08-29 17:38:37.687446: I tensorflow/stream_executor/cuda/cuda_dnn.cc:368] Loaded cuDNN version 8400<br>
+Saving MP4`;
+      }
+
+       let falseLink8 = document.querySelector(".false-link8");
+    falseLink8.addEventListener("click", changeInner8);
+
+     function changeInner8(event) {
+        event.preventDefault();
+        let falseLink = document.querySelector(".false-link8");
+        falseLink.outerHTML = `Running predict()...<br>
+Using seed: 30329<br>
+Generating initial latents for 20 steps<br>
+Generating first frame<br>
+Generating last frame<br>
+Generating frame 1<br>
+Generating frame 2<br>
+Generating frame 3<br>
+Generating frame 4<br>
+Generating frame 5<br>
+Generating frame 6<br>
+Generating frame 7<br>
+Generating frame 8<br>
+Generating frame 9<br>
+Generating frame 10<br>
+Generating frame 11<br>
+Generating frame 12<br>
+Generating frame 13<br>
+Generating frame 14<br>
+Generating frame 15<br>
+Generating frame 16<br>
+Generating frame 17<br>
+Generating frame 18<br>
+Interpolating images with FILM<br>
+2022-08-29 18:08:24.910902: I tensorflow/stream_executor/cuda/cuda_dnn.cc:368] Loaded cuDNN version 8400<br>
+Saving MP4`;
+      }
+
+
+        let falseLink9 = document.querySelector(".false-link9");
+    falseLink9.addEventListener("click", changeInner9);
+
+     function changeInner9(event) {
+        event.preventDefault();
+        let falseLink = document.querySelector(".false-link9");
+        falseLink.outerHTML = `Running predict()...<br>
+Using seed: 52129<br>
+Generating initial latents for 4 steps<br>
+Generating first frame<br>
+Generating last frame<br>
+Generating frame 1<br>
+Generating frame 2<br>
+Generating frame 3<br>
+Generating frame 4<br>
+Generating frame 5<br>
+Generating frame 6<br>
+Generating frame 7<br>
+Generating frame 8<br>
+Generating frame 9<br>
+Generating frame 10<br>
+Generating frame 11<br>
+Generating frame 12<br>
+Generating frame 13<br>
+Generating frame 14<br>
+Generating frame 15<br>
+Generating frame 16<br>
+Generating frame 17<br>
+Generating frame 18<br>
+Generating frame 19<br>
+Generating frame 20<br>
+Generating frame 21<br>
+Generating frame 22<br>
+Generating frame 23<br>
+Interpolating images with FILM<br>
+Saving MP4`;
+      }
